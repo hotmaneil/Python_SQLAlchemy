1e652ce79c3b3dd1784bea74dd10976f939697f5
hotmaneil <hotmaneil@gmail.com>
2025-07-22 16:26:17 +0800

    改成- %(key)s 是命名參數（搭配 dict）的寫法

--- queryBypymsql.py
+++ queryBypymsql.py
@@ -7,14 +7,15 @@ select
 	departments.name as departmentName
 from employees
 inner join departments on employees.department_id=departments.id 
-where employees.first_name like %s and employees.department_id=%s
+where employees.first_name like %(firstName)s and employees.department_id=%(departmentId)s
 
 """
 
-# params = {"employees.first_name": "%n%"}
-params = ("%j%", 1,)
+params = {
+    "firstName": "%n%",
+    "departmentId": 1
+}
 employeeList = query(sql, params, model=employeeViewModel)
 
-# print('employeeList', employeeList)
 for item in employeeList:
     print('employee item', item)
